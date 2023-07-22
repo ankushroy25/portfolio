770ab6ed8d306f96b956383f89c55ab8a0c039a4
Ankush Roy <imankushroy@gmail.com>
2023-07-23 02:24:14 +0530

    Added scroll to top button

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 200) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <>
+      <div
+        className={`fixed bottom-12 right-8 bg-teal-200 text-blue-700 border-4 border-blue-500 rounded-full p-2 cursor-pointer transition-opacity duration-300 ${
+          isVisible ? "opacity-100" : "opacity-0"
+        }`}
+        onClick={scrollToTop}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M5 10l7-7m0 0l7 7m-7-7v18"
+          />
+        </svg>
+      </div>
+    </>
+  );
+};
+
+export default ScrollToTop;
